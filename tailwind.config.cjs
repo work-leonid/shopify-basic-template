0e21eeaeadbb297584bd11679fabd8659eed818c
Leonid Nazarov <work.leonid@gmail.com>
2022-12-01 13:23:08 +0300

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'custom': ['Sora', 'sans-serif']
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 			}
